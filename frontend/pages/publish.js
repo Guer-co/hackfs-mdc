ce492b0d36e3e45aeec3452efb53fd4bd7bafa69
mastguer <mast@guer.co>
2020-07-15 23:49:52 -0700

    most functions complete, wizard started, start to cleanup warnings

--- frontend/pages/publish.js
+++ frontend/pages/publish.js
@@ -8,69 +8,110 @@ import buffer from 'buffer';
 import GatewayContractObjSetup from '../utils/GatewayConstructor';
 
 const Publish = () => {
-  const [{ dapp }, dispatch] = useStateValue();
-  const [loading, setLoading] = useState(false);
-  const [file, setFile] = useState('');
-  const [filename, setFilename] = useState('');
-  const [filehash, setFilehash] = useState('');
-  const [errorMessage, setError] = useState('');
-  const GatewayContractObj = GatewayContractObjSetup(dapp.web3);
+    const [{ dapp }, dispatch] = useStateValue();
+    const [loading, setLoading] = useState(false);
+    const [file, setFile] = useState('');
+    const [filename, setFilename] = useState('');
+    const [filehash, setFilehash] = useState('');
+    const [errorMessage, setError] = useState('');
+    const [mycontract, setMycontract] = useState('');
+    const [contentcount, setContentcount] = useState(0);
+    const [publishedcontent, setPublishedcontent] = useState('');
+    const [allcontent, setAllcontent] = useState([]);
+    
+    const GatewayContractObj = GatewayContractObjSetup(dapp.web3);
 
-  const uploadToIPFS = async () => {
-    setLoading(true);
-    const data = new FormData();
-    const file = document.getElementById('data_file').files[0];
-    data.append('file', file);
-    setFilename(file.name);
-    fetch('http://localhost:8888/api/ipfs', {
-      body: data,
-      method: 'POST'
-    })
-      .then((res) => res.json())
-      .then((res) => {
-        console.log(res);
-        setFilehash(res);
-        setLoading(false);
-      })
-      .catch((err) => {
-        setError(err.message);
-        setTimeout(() => setError(''), 3000);
-      });
-  };
-
-  const ping = async () => {
-    try {
-      fetch('http://localhost:8888/api/ping')
+    const uploadToIPFS = async () => {
+        setLoading(true);
+        const data = new FormData();
+        const file = document.getElementById('data_file').files[0];
+        data.append('file', file);
+        setFilename(file.name);
+        fetch('http://localhost:8888/api/ipfs', {
+        body: data,
+        method: 'POST'
+        })
         .then((res) => res.json())
-        .then((res) => console.log(res));
-    } catch (err) {
-      setError(err.message);
-      setTimeout(() => setError(''), 3000);
-    }
-  };
+        .then((res) => {
+            console.log(res);
+            setFilehash(res);
+            setLoading(false);
+        })
+        .catch((err) => {
+            setError(err.message);
+            setTimeout(() => setError(''), 3000);
+        });
+    };
 
-  const enterGateway = async () => {
+    const enterGateway = async () => {
     try {
-      await GatewayContractObj.methods
+        await GatewayContractObj.methods
         .createSimple()
         .send({ from: dapp.address })
         .on('transactionHash', (hash) => {
-          dispatch({
+            dispatch({
             type: 'SET_CURRENTLY_MINING',
             payload: true
-          });
+            });
         })
         .on('receipt', (hash) => {
-          dispatch({
+            dispatch({
             type: 'SET_CURRENTLY_MINING',
             payload: false
-          });
+            });
         });
     } catch (err) {
-      setError(err.message);
-      setTimeout(() => setError(''), 3000);
+        setError(err.message);
+        setTimeout(() => setError(''), 3000);
     }
-  };
+    window.location.reload(false);
+
+};
+
+    const addContentToContract = async () => {
+        console.log(mycontract + ' ' + filehash + ' ' + filename + ' ' + dapp.address);
+        await GatewayContractObj.methods.doAddContent(mycontract, filehash, filename).send({ from: dapp.address })
+            .then(function(result){
+                window.location.reload(false);
+            }).catch(function(error){
+                console.log(error);
+        });
+    }
+
+useEffect(() => {
+    
+    const loadGatewayData = async () => {
+        if (dapp.address && mycontract === '') {
+            setLoading(true);
+            //need to fix this line to only look for message sender
+            const contractfetch = await GatewayContractObj.methods.getUserContracts(dapp.address).call({from:dapp.address});
+            setMycontract(contractfetch[0]);
+            setLoading(false);
+        }
+        if (dapp.address && mycontract && publishedcontent === ''){
+            setLoading(true);
+            const contentcount = await GatewayContractObj.methods.doGetContentCount(mycontract).call({from:dapp.address});
+            setContentcount(contentcount);
+            setLoading(false);
+        }
+        if (contentcount > 0) {
+            const contentdetails = async () => {
+            let temparray = [];
+            for (let i = 0;i < contentcount;i++){
+                await GatewayContractObj.methods.doGetContent(mycontract, i).call({from: props.myaccount})
+                .then(function(result){
+                    //console.log(result);
+                    temparray.push(result);
+                });
+            }
+            setAllcontent(temparray);
+            }
+            contentdetails();
+        }
+    }
+    loadGatewayData();
+    setLoading(false);
+},[mycontract]);
 
   return (
     <Layout style={{ backgroundColor: '#041727' }}>
@@ -81,26 +122,35 @@ const Publish = () => {
         <Grid.Column>
           <div style={{ marginTop: '100px' }}>
             <hr />
-            <h3>
-              1. Create your smart contract (non-functioning, will be hidden if
-              contract is already created)
-            </h3>
-            <br />
-            <div>
-              {' '}
-              First, a prompt if you don't already have a smart contract
-              launched via our smart contract, we will auto detect this based on
-              the address you are using when you land on the site.
-            </div>
-            <br />
-            <Button onClick={enterGateway}>
-              Create your own smart contract on Pay3!
-            </Button>
+            {mycontract !== '' ?
+                <>
+                <h3>
+                1. Create your smart contract
+                </h3>
+                <br />
+                <div>
+                First, a prompt if you don't already have a smart contract
+                launched via our smart contract, we will auto detect this based on
+                the address you are using when you land on the site.
+                </div>
+                <br />
+                <Button onClick={enterGateway}>
+                Create your own smart contract on Pay3!
+                </Button>
+                </>
+            : 
+                <>
+                    <h3>
+                    1. Your Pay3 contract is created! Whats next???
+                    </h3>
+                </>
+            }
             <br />
             <hr />
-            <h3>2. Upload some piece of data to IPFS</h3>
-
+            <h3>{filehash === '' ? ("2. Upload some piece of data to IPFS") : ("2. File ready to upload, see step 3")}</h3>
             <br />
+            {filehash === '' ? (
+            <>
             <input
               id='data_file'
               type='file'
@@ -133,6 +183,9 @@ const Publish = () => {
             </label>
             <br />
             <br />
+            </>) : '' 
+            }
+
             <div id='ipfsdata'>
               {loading === true ? (
                 <>
@@ -149,10 +202,10 @@ const Publish = () => {
               )}
               {filehash !== '' ? (
                 <>
-                  <div id='name'>Name: {filename}</div>
-                  <div id='name'>IPFS HASH: {filehash}</div>
+                  <div id='name'><strong>Name:</strong> {filename}</div>
+                  <div id='name'><strong>IPFS HASH:</strong> {filehash}</div>
                   <div id='link'>
-                    Link to file:{' '}
+                    <strong>Link to file:</strong>{' '}
                     <a
                       target='_blank'
                       rel='no-follow'
@@ -167,19 +220,12 @@ const Publish = () => {
               )}
             </div>
             <br />
-            <br />
             <hr />
             <h3>
               3. Store that piece of data in your smart contract
-              (non-functioning)
             </h3>
             <br />
-            <div>
-              {' '}
-              Then some button here to push that information to your smart
-              contract, that will house all of your IPFS hashes.
-            </div>
-            <Button>Upload this file to your smart contract!</Button>
+            <Button onClick={addContentToContract}>Upload this file to your smart contract!</Button>
           </div>
         </Grid.Column>
       </Grid>
--- frontend/utils/GatewayConstructor.js
+++ frontend/utils/GatewayConstructor.js
@@ -1,5 +1,5 @@
 import { abi } from '../../build/contracts/Gateway.json';
-const address = '0xf2dA0229Aff5EB044D05451094FB3D64C312d0cE';
+const address = '0x3e196A7204a901CD24f5832f3E93844b98a985Bb';
 
 export default function gatewayContractSetup (dappWeb3) {
   return new dappWeb3.eth.Contract(abi, address)
