f5a8790eca5f965bd20a1d8f7e71ad04fd09b68e
Saurabh W <w.saurabh@protonmail.com>
2020-07-18 23:29:05 +0400

    added process.env

--- frontend/utils/PublisherConstructor.js
+++ frontend/utils/PublisherConstructor.js
@@ -1,6 +1,5 @@
 import { abi } from '../../build/contracts/Publisher.json';
-const address = '0x97B4b4CCe2483C868D32d3014c4FcE3Cf1b883bF';
 
 export default function publisherContractSetup (dappWeb3) {
-  return new dappWeb3.eth.Contract(abi, address)
+  return new dappWeb3.eth.Contract(abi, process.env.Publisher_Address)
 }
