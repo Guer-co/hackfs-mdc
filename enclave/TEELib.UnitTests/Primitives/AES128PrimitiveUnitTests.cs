702d3f68213ea34f70c6e11c5fb1772525e771b9
Moises Jaramillo <moisesj@Moisess-MBP.fios-router.home>
2020-07-21 14:35:36 -0400

    Simple Encryption/Decryption test

--- enclave/TEELib.UnitTests/Primitives/AES128PrimitiveUnitTests.cs
+++ enclave/TEELib.UnitTests/Primitives/AES128PrimitiveUnitTests.cs
@@ -1,4 +1,5 @@
 using System;
+using TEELib.Primitives;
 using Xunit;
 
 namespace TEELib.UnitTests.Primitives
@@ -6,9 +7,20 @@ namespace TEELib.UnitTests.Primitives
     public class AES128PrimitiveUnitTests
     {
         [Fact]
-        public void Test1()
+        public void TestEncryptDecryptText()
         {
+            const string TEST_TEXT = "This is a test";
 
+            var keyInfo = new KeyInfo();
+
+            var primitive = new AES128Primitive();
+
+            var cypherText = primitive.EncryptText(TEST_TEXT, keyInfo);
+            Assert.False(string.IsNullOrWhiteSpace(cypherText));
+            Assert.NotEqual(TEST_TEXT, cypherText);
+
+            var decryptedText = primitive.DecryptText(cypherText, keyInfo);
+            Assert.Equal(TEST_TEXT, decryptedText);
         }
     }
 }
