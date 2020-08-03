b1611712162b0b09a93b85b2ee9da7849709e69e
mastguer <mast@guer.co>
2020-08-03 15:34:07 -0700

    'backtonormal'

--- frontend/utils/getWeb3.js
+++ frontend/utils/getWeb3.js
@@ -1,11 +1,28 @@
 import Web3 from 'web3';
 
 let web3;
-const provider = new Web3.providers.HttpProvider(
-    `https://goreli.infura.io/v3/${process.env.INFURA_ID}` 
-);
-web3 = new Web3(provider);
 
+if (typeof window !== 'undefined') {
+  // Modern dapp browsers...
+  if (typeof window.ethereum !== 'undefined') {
+    window.ethereum.autoRefreshOnNetworkChange = false;
+    web3 = new Web3(window.ethereum);
+  }
+  // Legacy dapp browsers...
+  else if (typeof window.web3 !== 'undefined') {
+    // Use Mist/MetaMask's provider.
+    window.ethereum.autoRefreshOnNetworkChange = false;
+    web3 = new Web3(window.web3.currentProvider);
+    console.log('Injected web3 detected.');
+  }      
+}        
+// Fallback to Infura Node.
+else {
+  const provider = new Web3.providers.HttpProvider(
+    `https://goreli.infura.io/v3/${process.env.INFURA_ID}` 
+  );
+  web3 = new Web3(provider);
+}
 
 
 export default web3;
