e0de635a6f1dcfd09a344e43b5336e9aa76dc3b7
mastguer <mast@guer.co>
2020-08-03 14:28:47 -0700

    deploy on goerli so fleek will launch

--- frontend/utils/getWeb3.js
+++ frontend/utils/getWeb3.js
@@ -19,7 +19,7 @@ if (typeof window !== 'undefined') {
 // Fallback to Infura Node.
 else {
   const provider = new Web3.providers.HttpProvider(
-    `https://rinkeby.infura.io/v3/${process.env.INFURA_ID}` 
+    `https://goreli.infura.io/v3/${process.env.INFURA_ID}` 
   );
   web3 = new Web3(provider);
 }
